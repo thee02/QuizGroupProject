53d6568d29a87fbbb0fef22b718b941307f23a36
Nick Markworth <marcolus17@users.noreply.github.com>
2015-04-25 10:28:45 -0500

    Initial Commit
    Initial commit of the group project to the repository.

---
+++ quiz_report.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Quiz PHP</title>
+		<link rel="stylesheet" type="text/css" href="main.css" />
+	</head>
+	<body>
+		<?php
+			//Q1 answer was A
+			//Q2 answer was C
+			//Q3 answer was A
+			//Q4 answer was B
+			//Q5 answer was D
+
+			$totalScores = 0;
+
+
+			if (empty($_POST["name"])) {
+	
+				die ("Name is required. Please go back");
+			  } else {
+				$name = $_POST['name'];
+			  }
+
+
+
+			if(isset($_POST['Q1']))
+			{
+				if($_POST['Q1'] == "A")
+				{
+					$totalScores++;
+				}
+
+			}
+
+			if(isset($_POST['Q2']))
+			{
+				if($_POST['Q2'] == "C")
+				{
+					$totalScores++;
+				}
+
+			}
+			if(isset($_POST['Q3']))
+			{
+				if($_POST['Q3'] == "A")
+				{
+					$totalScores++;
+				}
+
+			}
+			if(isset($_POST['Q4']))
+			{
+				if($_POST['Q4'] == "B")
+				{
+					$totalScores++;
+				}
+
+			}
+			if(isset($_POST['Q5']))
+			{
+				if($_POST['Q5'] == "D")
+				{
+					$totalScores++;
+				}
+
+			}
+
+
+			echo "Name:". $name ."<br />"."<br />";
+			echo "Total Score: $totalScores points.";
+
+		?>
+	</body>
+</html>
